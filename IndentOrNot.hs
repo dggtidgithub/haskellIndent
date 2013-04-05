0c2e39f09300cb5897a9d6213768c2709009020d
David Guijarro <dgg@tid.es>
2013-04-05 08:45:05 +0200

    [REFACTOR] Reformulation of correct into correctIndent and correctDedent

--- IndentOrNot.hs
+++ IndentOrNot.hs
@@ -10,20 +10,27 @@ spaceTokens = map spaces . (filter ((/= ""). (dropWhile (== ' ')))). lines
 
 -- given a list (in fact ordered stack) of opened indentations in current scope
 -- test if current indentation is correct
--- boolean first argument means we need to find head of third argument in second argument stack
--- (this is the case for the dedent)
-correct :: (Num a, Ord a) => Bool -> [a] -> [a] -> Bool
-correct _ [] (0:y)  = correct False [0] y
-correct _ [] (_:_) = False
-correct _ _ []  = True
-correct v (x:t) (y:u)
-	| (x < y) = (not v) && (correct False (y:x:t) u)
-	| x == y = correct False (x:t) u 
-	| x > y = correct True t (y:u) 
+correctIndent :: (Num a, Ord a) => [a] -> [a] -> Bool
+correctIndent [] (0:y)  = correctIndent [0] y
+correctIndent [] (_:_) = False
+correctIndent _ []  = True
+correctIndent (x:t) (y:u)
+	| x < y  = correctIndent (y:x:t) u  -- indentation increasing case
+	| x == y = correctIndent (x:t) u    -- indentation remains the same
+	| x > y  = correctDedent t (y:u)    -- dedent case
+
+correctDedent :: (Num a, Ord a) => [a] -> [a] -> Bool
+correctDedent [] _ = False
+correctDedent _ []  = False
+correctDedent (x:t) (y:u)
+	| x < y  = False                    -- did not find the indentation level
+	| x == y = correctIndent (x:t) u 	-- found, continue with indentation testing
+	| x > y  = correctDedent t (y:u)    -- not yet, go on dedenting
+
 
 -- apply tokenization and correctness test to an input text
 indents :: String -> Bool
-indents = (correct False []) . spaceTokens
+indents = (correctIndent []) . spaceTokens
 
 -- Unit tests stuff (only very basic)
 tests = 
